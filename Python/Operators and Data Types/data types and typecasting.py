e4bc3b545878fd4ac01579cbb4830f12cbbcfaed
amyra2013 <amyrachavan@gmail.com>
2024-11-12 17:19:26 +0530

    change

---
+++ Python/Operators and Data Types/data types and typecasting.py
@@ -0,0 +1,20 @@
+name= "Penguin"
+age= 15
+is_student= True
+weight= 38.5
+
+print("Name:", name)
+print("Data type of Name is", type(name))
+print("Age:", age)
+print("Data type of Age is", type(age))
+type(is_student)
+print("Weight:", weight)
+print("Data type of weight is", type(weight))
+
+print ("\n After Type Casting...")
+age=str(age)
+print(age)
+print("Data type of age is", type(age))
+weight=int(weight)
+print(weight)
+print("Data Type of Weight is", type(weight))
