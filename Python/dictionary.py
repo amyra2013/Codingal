9cc6d662ce724009a8ddeb54f4bb4f3274df56c4
amyra2013 <amyrachavan@gmail.com>
2024-12-05 20:22:48 +0530

    medjhdcvff vfg

---
+++ Python/dictionary.py
@@ -0,0 +1,11 @@
+my_dict={}
+print(my_dict)
+my_dict={'Name':'Jack', '1': '127839' }
+print(my_dict['1'])
+my_dict.pop('1')
+print(my_dict)
+my_dict={'Name' : 'John', 'Age' : 13 }
+my_dict['Address']='Uptown'
+print(my_dict)
+my_dict.clear()
+print(my_dict)
---
+++ Python/tuple.py
@@ -0,0 +1,9 @@
+myTup=(1, 2, "Hello!", 3.14159)
+print(myTup)
+tup2=(45, 69, [72, "Hi!"], 872)
+print(tup2[2][1])
+tup3=("a", "m", "y", "r", "a")
+print(tup3[2])
+print(tup3[1:3])
+for i in tup3:
+    print(i,end=" " ) 
