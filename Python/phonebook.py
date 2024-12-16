20bf4c43a1c3419412ebd4c1146946a5ac8c6434
amyra2013 <amyrachavan@gmail.com>
2024-12-16 16:30:34 +0530

    commit

--- Python/phonebook.py
+++ Python/phonebook.py
@@ -32,9 +32,47 @@ def initial_phonebook():
     print(phone_book)
     return(phone_book)
 
+def menu():
+    print("*************************************************************************************");print("\t\t\tSMARTPHONE DIRECTORY, flush=False");
+    print("*************************************************************************************");print("\tYou can now perform the following operations on this phonebook\n");
+    print("1. Add a new contact");
+    print("2. Remove an existing contact");
+    print("3. Delete all contacts")
+    print("4. Search for a contact")
+    print("5. Display all contacts")
+    print("6. Exit phonebook")
+
+    choice=int(input("Please enter your choice:" ))
+
+    return choice
+
+def add_contact(pb):
+   dip=[]
+   for i in range(len(pb[0])) :
+    if i==0:
+        dip.append(str(input("Enter name:"))) 
+    if i==1:
+        dip.append(int(input("Enter number:")))
+    if i==2:
+        dip.append(str(input("Enter e-mail address:")))
+    if i==3:
+        dip.append(str(input("Enter date of birth(dd/mm/yy):")))
+    if i==4:
+        dip.append(str.input("Enter category(Family/Firnds/Work/Others:)"))
+
+    pb.append(dip)
+
+    return pb 
+def remove_existing(pb):
+       query=str(input("Please enter the name of the contact you wish to remove:"))
+       temp=0
+       for i in range          
 
 
-            
+
+
+
+
 
 
 
