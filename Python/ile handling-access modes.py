93ecaafe0b8222090e32ab270fe552715137f423
amyra2013 <amyrachavan@gmail.com>
2025-01-07 16:31:03 +0530

    even moore changes

---
+++ Python/ile handling-access modes.py
@@ -0,0 +1,9 @@
+file04=open('Codingal.txt', 'w')
+file04.write('Students of Class 8')
+file04.write('Student #1: Anupama', 'Age: 13', 'Roll Number: 01')
+file04.write('Student #2: Bill', 'Age: 12', 'Roll Number: 02')
+file04.write('Student #3: Roy', 'Age: 13', 'Roll Number: 03')
+file04.write('Student #4: Meena', 'Age: 12', 'Roll Number: 04')
+
+
+
