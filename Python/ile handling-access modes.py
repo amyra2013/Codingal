3d4ed3d967e40b88a6cb80cdf07f09da6c4b6925
amyra2013 <amyrachavan@gmail.com>
2025-01-07 16:39:28 +0530

    change

--- Python/ile handling-access modes.py
+++ Python/ile handling-access modes.py
@@ -1,9 +1,9 @@
 file04=open('Codingal.txt', 'w')
 file04.write('Students of Class 8')
-file04.write('Student #1: AnupamaAge: 13 Roll Number: 01')
-file04.write('Student #2: Bill Age: 12 Roll Number: 02')
-file04.write('Student #3: Roy Age: 13 Roll Number: 03')
-file04.write('Student #4: Meena Age: 12 Roll Number: 04')
+file04.write('Student #1: Anupama\nAge: 13\nRoll Number: 01\n')
+file04.write('Student #2: Bill\n Age: 12\nRoll Number: 02\n')
+file04.write('Student #3: Roy\nAge: 13\nRoll Number: 03\n')
+file04.write('Student #4: Meena\nAge: 12\nRoll Number: 04\n')
 
 
 
