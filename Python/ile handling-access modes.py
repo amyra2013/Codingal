10f877d4eda8f2998ff8c4588ebcc5f8b8e9d963
amyra2013 <amyrachavan@gmail.com>
2025-01-07 16:40:00 +0530

    close

--- Python/ile handling-access modes.py
+++ Python/ile handling-access modes.py
@@ -4,6 +4,6 @@ file04.write('Student #1: Anupama\nAge: 13\nRoll Number: 01\n')
 file04.write('Student #2: Bill\n Age: 12\nRoll Number: 02\n')
 file04.write('Student #3: Roy\nAge: 13\nRoll Number: 03\n')
 file04.write('Student #4: Meena\nAge: 12\nRoll Number: 04\n')
-
+file04.close()
 
 
