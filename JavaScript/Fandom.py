a5c0a00fd4aa18942671572e305e27cdc855b125
amyra2013 <amyrachavan@gmail.com>
2025-11-21 20:14:21 +0530

    fandom

---
+++ JavaScript/Fandom.py
@@ -0,0 +1,15 @@
+Fandom=int(input('Enter the number of your favourite fan universe:1 for Harry Potter, 2 for PJO/HOO/TOA/Kane Chronicles/Magnus Chase, 3 for The Hunger Games'))
+if Fandom==1:
+    house=int(input('Enter your house, Potterhead, and welcome to Hogwarts: 1.Gryffindor, where dwell the brave at heart 2.Slytherin, a house of cunning and resourcefulness 3.Ravenclaw,a house of intelligence and creativity 4.Hufflepuff, a house industrious perseverance and people who never give up'))
+    if house==1:
+        print("Hello, Gryffindor! You're in some pretty good company: the likes of Harry Potter, Hermione Granger, the Weasleys, and Neville Longbottom!")
+        if house==2:
+            print("Hey there, cunning Slytherin! You've got to have some rather...devious friends: Draco Malfoy, Vincent Crabbe, Gregory Goyle, and Pansy Parkinson might just be among your top picks!")
+            if house==3:
+                print("Greetings, bright Ravenclaw! You're surrounded by geniuses and top-hole wizards like Luna Lovegood, Cho Chang, and Rowena Ravenclaw!")
+                if house==4:
+                    print("Hi, Hufflepuff! You have some extremely kind and self-sacrificing companions, including Cedric Diggory!")
+elif Fandom==2:
+    parent=int(input("Hey, half-blood! Who's your godly parent?(1.Athena 2.Dionysus 3.Poseidon 4.Aphrodite 5.Hephaestus 6.Apollo)"))
+    
+    
